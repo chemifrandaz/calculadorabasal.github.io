0c8f877454b2fab98776ea2475615c36df3c68b2
chemifrandaz <151792763+chemifrandaz@users.noreply.github.com>
2023-11-28 23:03:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let peso = document.getElementById("peso");
+let boton = document.getElementById("calular");
+let VOL = document.getElementById(volumen);
+
+boton.addEventListener("click", eventoBoton);
+
+function eventoBoton(){
+    let peso = dato.valueAsNumber;
+    let volumen;
+    if(peso<=30);{
+
+    }
+}
+{
+    volumen = holliday(peso);
+} else {
+    volumen = sc(peso);
+
+}
+console.log(volumen);
+
+
+
+
+function holliday(peso) {
+return peso + 1
+}
+
+function sc(peso){
+return * 2
+}
